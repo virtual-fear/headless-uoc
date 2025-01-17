46ae05edcbb854a99336f047916004dd7de86a0f
virtualfear <dendan116@gmail.com>
2025-01-17 16:56:02 -0500

    Moved BookInfo and BookOpen events in with their arguments.

--- Client/Networking/Arguments/Book/BookInfoEventArgs.cs
+++ Client/Networking/Arguments/Book/BookInfoEventArgs.cs
@@ -1,6 +1,8 @@
 ﻿namespace Client.Networking.Arguments;
 public sealed class BookInfoEventArgs : EventArgs
 {
+    [PacketHandler(0x66, length: -1, ingame: true)]
+    public static event PacketEventHandler<BookInfoEventArgs>? Update;
     public NetState State { get; }
     public bool Supported { get; } = false;
     internal BookInfoEventArgs(NetState state, PacketReader ip)
--- Client/Networking/Arguments/Book/BookOpenEventArgs.cs
+++ Client/Networking/Arguments/Book/BookOpenEventArgs.cs
@@ -1,6 +1,9 @@
 ﻿namespace Client.Networking.Arguments;
 public sealed class BookOpenEventArgs : EventArgs
 {
+    [PacketHandler(0xD4, length: -1, ingame: true)]
+    public static event PacketEventHandler<BookOpenEventArgs>? Update;
+
     public NetState State { get; }
     public bool Supported { get; } = false;
     public string Title { get; }
